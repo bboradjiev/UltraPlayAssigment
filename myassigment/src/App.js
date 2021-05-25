231028c6f13ef377f64e77e71fb02645f5941743
Bojidar Boradjiev <bojidarboradjiev@gmail.com>
2021-05-25 11:21:34 +0300

    changes

--- myassigment/src/App.js
+++ myassigment/src/App.js
@@ -4,55 +4,58 @@ import RenderDataSortedByDate from "./Component/RenderDataSortedByDate";
 import RenderDataSortedByLeague from "./Component/RenderDataSortedByLeague";
 import { getData } from "./utils";
 import Loader from "react-loader-spinner";
+import { dummyData } from "../src/dummyData";
 
 function App() {
   const [data, setData] = useState([]);
   const [toggleBtn, setToggleBtn] = useState(true);
   const [spinner, setSpinner] = useState(true);
-
+  console.log(dummyData);
   useEffect(() => {
     setTimeout(() => {
-      const fetchedData = getData();
-      fetchedData.then(function (result) {
-        setData(result);
-        setSpinner(!spinner);
-      });
+      setData(dummyData);
+      setSpinner(!spinner);
+      // const fetchedData = getData();
+      // fetchedData.then(function (result) {
+      //   setData(result);
+      //   setSpinner(!spinner);
+      // });
     }, 500);
     setSpinner(spinner);
   }, []);
   //creating new Array with chosen data for easier handling
   const newDataWithDates = [];
-  for (let i = 0; i < data.length; i++) {
-    let gameName = data[i].$.Name.split(",")[0];
-    let leagueName = data[i].$.Name.split(", ")[1];
-    let matchesData = data[i].Match;
+  // for (let i = 0; i < data.length; i++) {
+  //   let gameName = data[i].$.Name.split(",")[0];
+  //   let leagueName = data[i].$.Name.split(", ")[1];
+  //   let matchesData = data[i].Match;
 
-    for (let j = 0; j < matchesData.length; j++) {
-      const match = {};
-      match.gameName = gameName;
-      match.leagueName = leagueName;
-      match.matchID = matchesData[j].$.ID;
-      match.matchName = matchesData[j].$.Name;
-      match.startDate = matchesData[j].$.StartDate;
-      match.bet =
-        matchesData[j].Bet === undefined
-          ? undefined
-          : matchesData[j].Bet[0].Odd[0].$.Value;
-      match.odd =
-        matchesData[j].Bet === undefined
-          ? undefined
-          : matchesData[j].Bet[0].Odd[1] === undefined
-          ? undefined
-          : matchesData[j].Bet[0].Odd[1].$.Value;
+  //   for (let j = 0; j < matchesData.length; j++) {
+  //     const match = {};
+  //     match.gameName = gameName;
+  //     match.leagueName = leagueName;
+  //     match.matchID = matchesData[j].$.ID;
+  //     match.matchName = matchesData[j].$.Name;
+  //     match.startDate = matchesData[j].$.StartDate;
+  //     match.bet =
+  //       matchesData[j].Bet === undefined
+  //         ? undefined
+  //         : matchesData[j].Bet[0].Odd[0].$.Value;
+  //     match.odd =
+  //       matchesData[j].Bet === undefined
+  //         ? undefined
+  //         : matchesData[j].Bet[0].Odd[1] === undefined
+  //         ? undefined
+  //         : matchesData[j].Bet[0].Odd[1].$.Value;
 
-      newDataWithDates.push(match);
-    }
-  }
+  //     newDataWithDates.push(match);
+  //   }
+  // }
   //sorting data so that it is desplayed by date
   const sortedByMatchStartDate = newDataWithDates.sort(function (d1, d2) {
     return new Date(d1.startDate) - new Date(d2.startDate);
   });
-  console.log(sortedByMatchStartDate);
+  console.log(toggleBtn);
 
   return (
     <div className="App">
@@ -79,11 +82,11 @@ function App() {
 
       <div>
         {toggleBtn === true
-          ? sortedByMatchStartDate.map((item) => (
-              <RenderDataSortedByDate кеу={item.matchID} {...item} />
+          ? dummyData.map((item, i) => (
+              <RenderDataSortedByDate кеу={i} {...item} />
             ))
-          : sortedByMatchStartDate.map((item) => (
-              <RenderDataSortedByLeague key={item.matchID} {...item} />
+          : dummyData.map((item, i) => (
+              <RenderDataSortedByLeague key={i} {...item} />
             ))}
       </div>
     </div>
